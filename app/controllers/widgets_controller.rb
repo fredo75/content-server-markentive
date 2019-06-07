0f59d3892e7157202a00eb37ba52057ec7bdbaf6
frederic curier <frederic.curier@gmail.com>
2019-06-07 17:06:05 +0200

    setup primary iframe ok for heroku

---
+++ app/controllers/widgets_controller.rb
@@ -0,0 +1,38 @@
+class WidgetsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  after_action :allow_iframe
+  layout 'widget_layout'
+
+  def widget
+    respond_to do |format|
+      format.html do
+        # load data to show in the view
+        # @data = ...
+        render :widget
+      end
+      format.js do
+        render :widget
+      end
+    end
+  end
+
+  def test_widget
+    render :test_widget, layout: false
+  end
+
+  private
+
+  def allow_iframe
+    response.headers.except! 'X-Frame-Options'
+  end
+
+  def get_rumours
+    if params[:id]
+      Rumour.displayed.where(id: params[:id])
+    else
+      Rails.cache.fetch('rumours_for_widget', expires_in: 5.minutes) do
+       Rumour.order(timestamp: :desc).displayed.limit(5)
+      end
+    end
+  end
+end
