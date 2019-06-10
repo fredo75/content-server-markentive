eabd7be1586eee3adffc3f57b4d60a22b3d64f63
frederic curier <frederic.curier@gmail.com>
2019-06-10 14:57:11 +0200

    fix appid env

---
+++ app/javascript/components/test.js
@@ -0,0 +1,6 @@
+function test () {
+
+  console.log("je suis là")
+}
+
+export {test};
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,6 @@
 import "bootstrap";
+
+import{algolia} from '../components/algolia.js.erb';
+ if (document.getElementById('my-widget')){
+  algolia();
+ }
