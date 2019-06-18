10d75f58d4a5df5683a3058d6d40eebf5a0c6bfb
frederic curier <frederic.curier@gmail.com>
2019-06-18 23:29:02 +0200

    setup config css4

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,4 @@ import{algolia} from '../components/algolia.js.erb';
  if (document.getElementById('my-widget')){
   algolia();
  }
-import{style_widget} from '../components/style_widget.js';
- if (document.getElementById('my-widget')){
-  style_widget();
- }
+
